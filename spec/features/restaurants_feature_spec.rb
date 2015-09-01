5e5cc56491cfbf37572de97000e596e377d1c063
Diego Regules <diegoregules@gmail.com>
2015-09-01 14:03:21 +0100

    added feature testing

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'no restaurants yet'
+      expect(page).to have_link 'add restaurant'
+    end
+  end
+end
