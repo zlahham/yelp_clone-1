d32bec84b8e34d993cd353d79f9b5c1c50c94508
Diego Regules <diegoregules@gmail.com>
2015-09-01 15:49:47 +0100

    db schema and migration run

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'restaurants' do
       expect(page).to have_link 'add restaurant'
     end
   end
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('no restaurants yet')
+    end
+  end
 end
