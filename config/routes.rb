a6b92f64883a869e7a2fe79b359d964a34410abb
Zaid Al Lahham <zlahham@gmail.com>
2015-09-03 10:10:34 +0100

    Moving into OmniAuth Login

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,6 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
-
-
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
