f3bd263eba7fc27d4a77759671ac1e89265ee6ca
Retesh <retesh.bajaj@gmail.com>
2015-09-04 10:54:21 +0100

    just before reviewed rest table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe User, type: :model do
   it { is_expected.to have_many :restaurants}
+  it { is_expected.to have_many :reviews}
+  it { is_expected.to have_many :reviewed_restaurants}
+
 end
